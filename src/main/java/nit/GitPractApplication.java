dfb16f3681b82ca7bcd318d203354b873157498f
sachin <jadhavsachin5858@gmail.com>
2023-06-10 18:45:03 +0530

    gitpract from intellij

---
+++ src/main/java/nit/GitPractApplication.java
@@ -0,0 +1,13 @@
+package nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPractApplication.class, args);
+	}
+
+}
