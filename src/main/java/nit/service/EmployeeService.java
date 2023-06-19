be4fa68251b50632d0fc8e994b3793521a6ff2a1
sachin <jadhavsachin5858@gmail.com>
2023-06-19 19:34:12 +0530

    save() and fetchings() method added

---
+++ src/main/java/nit/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package nit.service;
+
+public class EmployeeService implements IEmployeeService{
+    @Override
+    public void save() {
+
+    }
+
+    @Override
+    public void fetching() {
+
+    }
+}
